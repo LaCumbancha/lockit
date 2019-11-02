68ba73b0728b08d531571a81c6bba00d137e2ca6
Cristian Raña <cristian.rana@despegar.com>
2019-11-01 22:24:46 -0300

    Bootstrapping

--- README.md
+++ README.md
@@ -6,12 +6,22 @@ To begin, let's install the latest version of the Ionic CLI.
 npm install -g ionic@latest
 ```
 
-From here, the global command ionic will allow for the management of the React project with Ionic and any other dependencies. To run the project, execut the following command:
+From here, the global command ionic will allow for the management of the React project with Ionic and any other dependencies. To run the project, execute the following command:
 ```
 ionic serve
 ```
 
-The server will be listening at port `8100`.
+#### Web
+
+The server will be listening at `localhost:8100`. Just access with your favorite browser.
+
+#### Mobile
+
+To run the application, you must first download the **Ionic Devapp** for your mobile OS (you can find it at the [PlayStore](https://play.google.com/store/apps/details?id=io.ionic.devapp) or at the [AppStore](https://apps.apple.com/us/app/ionic-devapp/id1233447133)). Once you got the app, be sure to connect both your mobile and the server on the same WiFi network. Then, open the **Ionic Devapp** and set the server address manually in the menu:
+* Address: 192.168.1.118
+* Port: 8100
+
+Finally, just find your application in the list, and enjoy!
 
 #### Common Issues
 Listen uses inotify by default on Linux to monitor directories for changes. It's not uncommon to encounter a system limit on the number of files you can monitor. If while running the server you get the following error:
--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
 import React from 'react';
 import { Redirect, Route } from 'react-router-dom';
-import { IonApp, IonRouterOutlet } from '@ionic/react';
+import {
+  IonApp,
+  IonIcon,
+  IonLabel,
+  IonRouterOutlet,
+  IonTabBar,
+  IonTabButton,
+  IonTabs
+} from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
-import Home from './pages/Home';
+import { apps, flash, send } from 'ionicons/icons';
+import Tab1 from './pages/Tab1';
+import Tab2 from './pages/Tab2';
+import Tab3 from './pages/Tab3';
+import Details from './pages/Details';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -26,10 +38,29 @@ import './theme/variables.css';
 const App: React.FC = () => (
   <IonApp>
     <IonReactRouter>
-      <IonRouterOutlet>
-        <Route path="/home" component={Home} exact={true} />
-        <Route exact path="/" render={() => <Redirect to="/home" />} />
-      </IonRouterOutlet>
+      <IonTabs>
+        <IonRouterOutlet>
+          <Route path="/tab1" component={Tab1} exact={true} />
+          <Route path="/tab2" component={Tab2} exact={true} />
+          <Route path="/tab2/details" component={Details} />
+          <Route path="/tab3" component={Tab3} />
+          <Route path="/" render={() => <Redirect to="/tab1" />} exact={true} />
+        </IonRouterOutlet>
+        <IonTabBar slot="bottom">
+          <IonTabButton tab="tab1" href="/tab1">
+            <IonIcon icon={flash} />
+            <IonLabel>Tab One</IonLabel>
+          </IonTabButton>
+          <IonTabButton tab="tab2" href="/tab2">
+            <IonIcon icon={apps} />
+            <IonLabel>Tab Two</IonLabel>
+          </IonTabButton>
+          <IonTabButton tab="tab3" href="/tab3">
+            <IonIcon icon={send} />
+            <IonLabel>Tab Three</IonLabel>
+          </IonTabButton>
+        </IonTabBar>
+      </IonTabs>
     </IonReactRouter>
   </IonApp>
 );
