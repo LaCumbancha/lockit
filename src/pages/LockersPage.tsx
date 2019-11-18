219755e6a036ab861366c514cb9f1ad960a02fab
RodrigoDeRosa <rodrigo.derosa@despegar.com>
2019-11-18 19:58:59 -0300

    added title to lockers page

--- src/pages/LockersPage.tsx
+++ src/pages/LockersPage.tsx
@@ -1,22 +1,23 @@
 import React from 'react';
-import { IonContent, IonPage } from '@ionic/react';
+import {IonContent, IonHeader, IonPage} from '@ionic/react';
 import Bag from "../components/Bag/Bag";
 import './main.css'
 import MainIcons from "../components/MainIcons/MainIcons";
 
 const LockersPage: React.FC = () => {
     return (
-    <IonPage>
-      <IonContent>
-        <Bag
-            name="Mochila de trabajo"
-            location="Starbucks (San Telmo) - Defensa 1102"
-            transporting={false}
-        />
-        <MainIcons/>
-      </IonContent>
-    </IonPage>
-  );
+        <IonPage>
+            <IonContent>
+                <span className="main-title">Mis Lockers</span>
+                <Bag
+                    name="Mochila de trabajo"
+                    location="Starbucks (San Telmo) - Defensa 1102"
+                    transporting={false}
+                />
+                <MainIcons/>
+            </IonContent>
+        </IonPage>
+    );
 };
 
 export default LockersPage;
