98bf2c62ab904116476f8727cd64263cbf779c7c
Cristian Raña <cristian.rana@despegar.com>
2019-11-20 22:16:35 -0300

    QR Modal added

---
+++ src/components/QR/QRLockerModal.tsx
@@ -0,0 +1,42 @@
+import React, {Component} from "react";
+import './QRLockerModal.css'
+
+type ModalProps = {
+    show: boolean
+};
+
+type State = {
+    show: boolean
+};
+
+
+export default class QRLockerModal extends Component<ModalProps,State> {
+
+    constructor({props}: { props: any }) {
+        super(props);
+        this.state = {show: false};
+    }
+
+    render() {
+        const QR = "/assets/QR.jpg";
+        const showHideClassName = this.state.show ? "modal display-block" : "modal display-none";
+
+        return (
+            <div className={showHideClassName} style={{zIndex:100}}>
+                <section className="modal-main">
+                    <button className="close-button" onClick={this.hideModal}>X</button>
+                    <img className="QR-image" src={QR} alt={""}/>
+                    <div className="QR-text">¡Escanea este QR para abrir tu locker!</div>
+                </section>
+            </div>
+        );
+    }
+
+    hideModal = () => {
+        this.setState({ show: false });
+    };
+
+    showModal = () => {
+        this.setState({show: true});
+    }
+};
