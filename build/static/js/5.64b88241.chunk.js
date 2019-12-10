a8802909c078650c6952d7156fd17705cdd68659
Facundo Guerrero <facundo.guerrero@despegar.com>
2019-12-10 14:06:45 -0300

    elimino los archivos buildeados

--- build/static/js/5.64b88241.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonplockit=this.webpackJsonplockit||[]).push([[5],{231:function(n,t,o){"use strict";o.r(t),o.d(t,"startStatusTap",(function(){return i}));var e=o(39),i=(o(13),function(){var n=window;n.addEventListener("statusTap",(function(){Object(e.g)((function(){var t=n.innerWidth,o=n.innerHeight,i=document.elementFromPoint(t/2,o/2);if(i){var c=i.closest("ion-content");c&&c.componentOnReady().then((function(){Object(e.m)((function(){return c.scrollToTop(300)}))}))}}))}))})}}]);
-//# sourceMappingURL=5.64b88241.chunk.js.map
